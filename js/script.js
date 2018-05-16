1812874d43ca9bbc0185095981facd49a0f62555
Elton Memishaj <eltoni.memishi@live.com>
2018-05-16 22:42:55 +0200

    Added onclick listener for settings button.

--- js/script.js
+++ js/script.js
@@ -2,5 +2,9 @@
 $(function() {
   $('header > div.title > i.material-icons').click(function(e) { $('body').toggleClass('sidebar-closed'); });
   $('.main-content').click(function() { $('body').addClass('sidebar-closed'); });
-  $('header > div.right-nav > i.material-icons').click(function() { window.close(); });
+  $('header > div.right-nav > i.material-icons:first-child').click(function(e) {
+    $(e.target).toggleClass('rotated');
+    $('header > div.right-nav > div.settings').toggleClass('closed');
+  });
+  $('header > div.right-nav > i.material-icons:last-child').click(function() { window.close(); });
 });
