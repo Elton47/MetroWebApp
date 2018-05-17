c43e4342b694ee00aff5bd10f4cd223078dc1075
Elton Memishaj <eltoni.memishi@live.com>
2018-05-17 21:57:38 +0200

    Added colorPaletteSequencer recursive function to toggle hover state.

--- js/script.js
+++ js/script.js
@@ -7,4 +7,10 @@ $(function() {
     $('header > div.right-nav > div.settings').toggleClass('closed').children().click(function() { $('body').toggleClass('black-theme'); });
   });
   $('header > div.right-nav > i.material-icons:last-child').click(function() { window.close(); });
+  colorPaletteSequencer(0, $('div.color-palette'));
 });
+function colorPaletteSequencer(index, elements) {
+  elements.removeClass('hover'); 
+  elements.eq(index).addClass('hover');
+  setTimeout(function() { colorPaletteSequencer(index === 5 ? 0 : ++index, elements); }, 1500);
+}
