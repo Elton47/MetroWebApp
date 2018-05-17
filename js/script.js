1d96890a76144f3ad6666c43a4a5bec2765feab5
Elton Memishaj <eltoni.memishi@live.com>
2018-05-17 14:24:02 +0200

    Removed main onclick, added .tint onclick listener instead.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 /// <reference path="_references.js" />
 $(function() {
   $('header > div.title > i.material-icons').click(function(e) { $('body').toggleClass('sidebar-closed'); });
-  $('.main-content').click(function() { $('body').addClass('sidebar-closed'); });
+  $('main > .tint').click(function() { $('body').addClass('sidebar-closed'); });
   $('header > div.right-nav > i.material-icons:first-child').click(function(e) {
     $(e.target).toggleClass('rotated');
     $('header > div.right-nav > div.settings').toggleClass('closed').children().click(function() { $('body').toggleClass('black-theme'); });
