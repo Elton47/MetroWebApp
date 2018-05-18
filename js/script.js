acb96e2b96a29c191c1dfac0472923dc7fbd6c7d
Elton Memishaj <eltoni.memishi@live.com>
2018-05-19 00:19:59 +0200

    Added toast function. Added onclick listener for fab to create test toast.

--- js/script.js
+++ js/script.js
@@ -7,10 +7,17 @@ $(function() {
     $('header > div.right-nav > div.settings').toggleClass('closed').children().click(function() { $('body').toggleClass('black-theme'); });
   });
   $('header > div.right-nav > i.material-icons:last-child').click(function() { window.close(); });
+  $('.main-top-wrapper > .fab').click(function(e) { toast(4000, 'Test', 'warning', 'primary-color'); });
   colorPaletteSequencer(0, $('div.color-palette'));
 });
 function colorPaletteSequencer(index, elements) {
   elements.removeClass('hover'); 
   elements.eq(index).addClass('hover');
   setTimeout(function() { colorPaletteSequencer(index === 5 ? 0 : ++index, elements); }, 1500);
+}
+function toast(duration, message, icon, color) {
+  $('.toast').remove();
+  $('body').append('<div class="toast shown ' + color + '">' + message + '</div>');
+  icon ? $('.toast').prepend('<i class="material-icons">' + icon + '</i>') : '';
+  setTimeout(function() { $('.toast').removeClass('shown'); }, duration);
 }
